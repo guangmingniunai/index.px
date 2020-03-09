c8c1e8788030dd76a8fb7cdd73c05b69ecab196d
guangmingniunai <56286369+guangmingniunai@users.noreply.github.com>
2020-03-09 19:31:05 +0800

    Create inde.d

---
+++ inde.d
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" >
+  <title>oppo</title>
+</head>
+
+<body background="C:\Users\Public\Pictures\Sample Pictures\1.jpg" style="background-repeat: no-repeat; background-size: 100% 100%; background-attachment: fixed;">
+  <hr />
+
+    <div style="letter-spacing: 5px;background-color: red;width:100px;margin: 0 auto">oppo</div>
+        <p>oppo watch 系列</p>
+          <b>【预定享立减100元优惠 | 晒单赢手表】eSIM独立通信 | AMOLED屏 | 双擎动力续航 | 长续航模式14天 | 运动健康伴侣 | Watch VOOC闪充 | 游泳级防水</b>
+          <img src="file:///C:/Users/Administrator/Desktop/202003060303275e61f8530c665.webp" width="400px" height="500px">
+          <a href="https://www.opposhop.cn/products/1174.html">OPPO Watch 41mm 粉金</a>
+          <hr />
+        <img src="C:\Users\Public\Pictures\Sample Pictures\202003060503515e62150328bd3.webp.jpg" width="1200px" height="600px">
+                <p>下载 OPPO 商城 APP</p>
+                <p>领新人专享礼包</p>
+          <img src="C:\Users\Public\Pictures\Sample Pictures\202003080903275e64f8d76fe0d.webp.jpg">
+</body>
+</html>
